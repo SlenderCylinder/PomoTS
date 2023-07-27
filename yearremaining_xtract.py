0477ef27e103a2972eea01d64c14d5414754b393
Chamith <cweerathunga@hotmail.com>
2023-07-27 15:33:33 +0530

    Delete yearremaining_xtract.py

--- yearremaining_xtract.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-
-auth = tweepy.OAuth2BearerHandler("AAAAAAAAAAAAAAAAAAAAAG9gmgEAAAAAD9ZI2liksJDvHMFC8mzv%2FoPFPPw%3Db1m3APslf9WTCkfQh2fVRUQlE7ZzSZpMWh9jyDpwjCObujHmwq")
-api = tweepy.API(auth, wait_on_rate_limit=True) 
-items = tweepy.Cursor(api.user_timeline, screen_name='year_remaining', count=2).items(1)
-
-for item in items:
-    print(item.text)
