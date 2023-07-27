187b460a42b1ed5f2007dec08f002c9c2f7d8892
Chamith <cweerathunga@hotmail.com>
2023-07-27 15:33:06 +0530

    Update yearprogress_xtract.py

--- yearprogress_xtract.py
+++ yearprogress_xtract.py
@@ -1,6 +1,6 @@
 import tweepy
 
-auth = tweepy.OAuth2BearerHandler("AAAAAAAAAAAAAAAAAAAAAG9gmgEAAAAAD9ZI2liksJDvHMFC8mzv%2FoPFPPw%3Db1m3APslf9WTCkfQh2fVRUQlE7ZzSZpMWh9jyDpwjCObujHmwq")
+auth = tweepy.OAuth2BearerHandler("TWITTER TOKEN GOES HERE")
 api = tweepy.API(auth, wait_on_rate_limit=True) 
 items = tweepy.Cursor(api.user_timeline, screen_name='progressbar202_', count=2).items(1)
 
